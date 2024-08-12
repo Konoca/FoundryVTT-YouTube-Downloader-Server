fbe8e73c387f8b8cfe6f1f86ff2fb58d652ba8af
Konoca <ichris170@gmail.com>
2024-08-12 15:02:28 -0400

    add dockerfile

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-from flask import Flask, request, jsonify  #, Response
+from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from youtubesearchpython import VideosSearch
 import yt_dlp
-# from urllib.parse import urlparse
-# import requests
 import shutil
 
 FORMAT = 'mp3'
-# FORMAT = 'ogg'
-PATH_TO_WORLDS = './worlds/'
+PATH_TO_WORLDS = '/worlds'
 MUSIC_DIR = 'music'
 
 app = Flask(__name__)
@@ -48,66 +45,6 @@ def search():
         })
 
     return jsonify(videos)
-
-
-# @app.route('/download/id', methods=['GET'])
-# @cross_origin()
-# def download():
-#     id = request.args.get('id')
-#
-#     results = VideosSearch(id, limit=1)
-#     result = results.result().get('result', [])
-#     url = result[0].get('link', '') if results != [] else f'ytsearch1:{id}'
-#
-#     data = ytdl.extract_info(url, download=False)
-#
-#     return jsonify({
-#         'url': (
-#             data['entries'][0]['url']
-#             if url.startswith('ytsearch1:')
-#             else data['url']
-#         ),
-#         'codec': FORMAT,
-#     })
-#
-#
-# @app.route('/download/url', methods=['GET'])
-# @cross_origin()
-# def download_url():
-#     url = request.args.get('url')
-#     data = ytdl.extract_info(url, download=False)
-#
-#     return jsonify({
-#         'id': data['id'],
-#         'title': data['title'],
-#         'url': data['url'],
-#         'codec': FORMAT,
-#     })
-#
-#
-# @app.route('/proxy', methods=['GET'])
-# @cross_origin()
-# def proxy():
-#     url = request.args.get('url')
-#     parsed = urlparse(url)
-#     domain = parsed.netloc.split('.')
-#     if len(domain) < 3 or domain[1] != 'googlevideo' or domain[2] != 'com':
-#         return 'Bad Request: Invalid URL', 400
-#
-#     try:
-#         response = requests.get(url, allow_redirects=False, stream=True)
-#         def generate():
-#             for chunk in response.raw.stream(decode_content=False):
-#                 yield chunk
-#     except Exception as e:
-#         print(e)
-#         return f'Error: {e}', 502
-#
-#     return Response(
-#         generate(),
-#         status=response.status_code,
-#         headers=dict(response.headers)
-#     )
 
 @app.route('/download', methods=['GET'])
 @cross_origin()
